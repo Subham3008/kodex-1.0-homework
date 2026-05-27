ac7748919fecb3abc8dd2e1071870896e2fe6528
Subham3008 <subhamsamanta065@gmail.com>
2026-05-27 17:49:34 +0530

    feat: config Database

--- 01-CRUD/server1/server.js
+++ 01-CRUD/server1/server.js
@@ -1,7 +1,12 @@
-const app = require("./src/app")
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 const port = process.env.PORT || 4000
 
+//connect to Database
+connectDB()
+
+//server start
 app.listen(port, ()=>{
   console.log(`Server is running on port ${port}`);
   
