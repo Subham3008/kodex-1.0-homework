4b15192d59f2e0a73bf376d98b1d6ca0ab89d5b0
Subham3008 <subhamsamanta065@gmail.com>
2026-05-27 20:37:49 +0530

    feat: config server and Database in server3

---
+++ 01-CRUD/server3/server.js
@@ -0,0 +1,13 @@
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+const port = process.env.PORT || 4000
+
+//connect to Database
+connectDB()
+
+//server start
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+
+})
