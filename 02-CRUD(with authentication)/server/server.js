7cb12d501b839687279e19698ab4863667ed95e7
Subham3008 <subhamsamanta065@gmail.com>
2026-05-28 16:13:52 +0530

    feat: config database in 02-crud

--- 02-CRUD(with authentication)/server/server.js
+++ 02-CRUD(with authentication)/server/server.js
@@ -1,7 +1,10 @@
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 const port = process.env.PORT || 4000
 
+//connect to Database
+connectDB()
 
 //server start
 app.listen(port, () => {
