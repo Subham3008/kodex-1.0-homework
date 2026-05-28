8710b312cd8a05d0593b589bf3939caa21a828d4
Subham3008 <subhamsamanta065@gmail.com>
2026-05-28 16:19:41 +0530

    feat: GEH class and middleware create in 02-crud

---
+++ 02-CRUD(with authentication)/server/src/middlewares/error.middleware.js
@@ -0,0 +1,15 @@
+const errorMiddlewarre = (err, req, res, next) => {
+
+  console.log("Api error from Api middleware", err);
+
+
+  let statusCode = err.statusCode || 500
+  let message = err.message || "Internal server error."
+
+  return res.status(statusCode).json({
+    message: message,
+  })
+
+}
+
+module.exports = errorMiddlewarre
