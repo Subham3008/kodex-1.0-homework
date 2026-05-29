6e79c496612332ea6cb15aa6f457ab38789beb1f
Subham3008 <subhamsamanta065@gmail.com>
2026-05-29 17:12:59 +0530

    feat(schema): create user and product schema in 03-Ecommerce

---
+++ 03-Ecommerce-PrReview/server/src/models/product.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    category: {
+      type: String,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productModel = mongoose.model("Product", productSchema);
+
+module.exports = productModel
---
+++ 03-Ecommerce-PrReview/server/src/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    refreshTokenHash: {
+      type: String,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel
